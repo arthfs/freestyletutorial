24e20aaf7c9fc6aa2b0ae6f3d71e4f0f222404fb
arthfs <arthursaintlouis96@gmail.com>
2025-08-05 22:06:01 -0400

    fixed more errors in glossary.tsx

--- src/app/glossary/page.tsx
+++ src/app/glossary/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Glossary() {
-  const ref = {'latw':'Lemmens around the world','hmatw':'homie mitch around the world','laatw':'lemmens abbas around the world','htatw':"homie touzani around the world",'atw':'around the world','tatw':'touzani around the world','lmatw':'lemmens mitch around the world',
+  const ref: { [x: string]: string  }  = {'latw':'Lemmens around the world','hmatw':'homie mitch around the world','laatw':'lemmens abbas around the world','htatw':"homie touzani around the world",'atw':'around the world','tatw':'touzani around the world','lmatw':'lemmens mitch around the world',
     'mgmatw' :'magellan mitch around the world','alatw':'alternative lemmens arround the world','mgatw':'magellan around the world','htaatw':'homie touzani abbas around the world',
     'amatw':'alternative mitch around the world','ahmatw':'alternative homie mitch around the world','atatw':'alternative touzani around the world','htw':'hop the world',
     'rlebatw':'reverse lebioda around the world','matw':'mitch around the world','aatw':'abbas around the world','ahtatw':'alternative homie touzani around the world'
