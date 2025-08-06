cfa3526881d4bab6b5316830037e5a1b273e1ce7
arthfs <arthursaintlouis96@gmail.com>
2025-08-06 19:30:50 -0400

    fixed problems in notfound.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
-      <p>{"The page you're looking for doesn't exist."}</p>
+      <h1>{"404 - Page Not Found"}</h1>
+      <p>{"The page you\'re looking for doesn\'t exist."}</p>
     </div>
   )
 }
